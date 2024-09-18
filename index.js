d765b3ad2e211e67f36551ea6e0239554fdf63b4
UsamDaw <usama2000dawod@gmail.com>
2024-09-18 15:05:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+/* Task JSON-file
+let file = `fotograf.json`
+
+fetch(file)
+.then(response => response.json())
+.then(data => {
+    // Spørsmål 1
+    // console.log(`i den filen har vi ${data.length} bilder`);
+    
+    // Spørsmål 2
+    // let array = []
+    // data.forEach(element => {
+    //     array.push(element.albumId)
+        
+    // });
+    // console.log(`i den listen er det ${Math.max(...array)} albumer`);
+    
+    
+    // Spørsål 3
+    // data.forEach(element => {
+    //     if (element.id == 238) {
+    //         console.log(element.title);
+            
+    //     }
+        
+    // });
+
+
+    // Spørsmål 4
+    // data.forEach(element => {
+    //     if (element.id == 55) {
+    //         console.log(element.url);
+    //         document.getElementById("s4").src = element.url;
+            
+    //     }
+    // });
+
+
+}
+)
+*/
+
+/* Task API
+let url = "https://jsonplaceholder.typicode.com/posts"
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+    
+// Task 1
+//     data.forEach(element => {
+//        if (element.id == 95) {
+//         console.log(element.title); 
+//       }
+//    });
+
+
+// Task 2
+//    data.forEach(element => {
+//      if (element.userId == 5) {
+//        console.log(element.body);
+//        document.getElementById("textBody").innerText = element.body;
+//      }
+//    });
+
+
+}
+)
+*/
